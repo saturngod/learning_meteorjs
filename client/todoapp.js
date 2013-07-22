bb137056b874d6c5337ee2c3db40ec6b5b68fba4
saturngod <saturngod@gmail.com>
2013-07-22 18:05:21 +0800

    change folder structure

---
+++ client/todoapp.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+  
+  Template.todos.todoList = function () {
+    return Todos.find();
+  };
+
+  Template.todoItem.itemID = function () {
+    return this._id;
+  };
+
+  Template.todos.events({
+    'click input#btnAdd' : function(){
+     insertNew();
+    },
+    'keypress input#todoText': function (evt) {
+      if (evt.which === 13) {
+        insertNew();
+      }
+    }
+  });
+  Template.todoItem.events({
+    'click input.done' : function(){
+      Todos.remove(this._id);
+    }
+  });
+
+ function insertNew() 
+ {
+     Todos.insert({name:$("#todoText").val()});
+     $("#todoText").val("");
+ }
+}
